6322b9c13f7a7f770a6dc177fb515200393ad429
Daniel <danielesteban2004@gmail.com>
2022-07-26 02:01:03 -0500

    new calculator

--- js/script.js
+++ js/script.js
@@ -1,255 +1,169 @@
-//Punto 1
-//Botones
-var one = document.getElementById("uno");
-var two = document.getElementById("dos");
-var three = document.getElementById("tres");
-var four = document.getElementById("cuatro");
-var five = document.getElementById("cinco");
-var six = document.getElementById("seis");
-var seven = document.getElementById("siete");
-var eight = document.getElementById("ocho");
-var nine = document.getElementById("nueve");
-var zero = document.getElementById("cero");
-var ce = document.getElementById("C");
-var ac = document.getElementById("AC");
-var prc = document.getElementById("porc");
-var daiv = document.getElementById("divi");
-var eq = document.getElementById("ig");
-var plus = document.getElementById("mas");
-var min = document.getElementById("men");
-var times = document.getElementById("mul");
-var point = document.getElementById("punto");
-
-//Elementos de operaciones
-var n1 = 0; //operador uno
-var n2 = 0; //operador dos
-var division = false;
-var suma = false;
-var resta = false;
-var multiplicacion = false;
-var pnt = false; //para poner el punto decimal solo una vez
-
-//Otros elementos
-var pantalla = document.getElementById("screen"); //el input que muestra todo el proceso que estamos realizando
-var ans1 = document.getElementById("resp1"); //h4 que muestra el resultado
-pantalla.innerText = "0";
-var prim; //posicion anterior, para guardar todo en un array
-var seg; //posicion siguiente
-var cantidad = []; //array que tiene todo lo que se ve en pantalla
-var i = 0; //acomulador para usar el array
-
-//local storage
-var beefore = document.getElementById("anteriores");
-var guardar = [];
-var k = 0;
-
-//Funciones
-
-function numero(a) { //mostrar los numeros en pantalla y guardarlos en variables
-    if (pantalla.value == "0") {
-        pantalla.value = a.value;
-        cantidad[0] = [a.value];
-        i = 1;
-    } else {
-        if (i == 1) {
-            prim = pantalla.value;
-            seg = a.value;
-            pantalla.value = prim + seg
-            cantidad[1] = pantalla.value;
-            i++;
-        } else if (i >= 2) {
-            prim = pantalla.value;
-            seg = a.value;
-            pantalla.value = prim + seg
-            cantidad[i] = pantalla.value;
-            i++
-        }
-
+// Numbers
+const botones = [...document.querySelectorAll(".num")];
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const simbolos = ["+", "-", "/", "*"]
+const numBut = botones.filter(element => {
+    if (numeros.includes(parseInt(element.value))) {
+        return element;
+    } else if (element.value == ".") {
+        return element;
     }
-
-}
-
-
-function operacion() {
-    if (n1 == 0) {
-        n1 = parseInt(pantalla.value);
-        pantalla.value = "0";
-        division = false;
-        multiplicacion = false;
-        suma = false;
-        resta = false;
-        pnt = false;
-    } else {
-        alert("Solo se puede usar una vez la operación")
-    }
-}
-
-
-
-//Operaciones
-prc.addEventListener("click", function () {
-    pantalla.value = pantalla.value / 100
-})
-
-daiv.addEventListener("click", function () {
-    operacion();
-    division = true;
-})
-
-plus.addEventListener("click", function () {
-    operacion();
-    suma = true;
-})
-
-min.addEventListener("click", function () {
-    operacion();
-    resta = true;
-
-})
-
-times.addEventListener("click", function () {
-    operacion();
-    multiplicacion = true;
-
-})
-
-
-
-eq.addEventListener("click", function () {
-    n2 = parseInt(pantalla.value);
-    if (division == true) {
-        if(n2==0){alert("No se puede dividir entre 0");}
-        else{
-            ans1.innerText = n1 / n2;
-            division=false;
-            guardar[k] = (n1+"/"+n2+"="+(n1/n2));
-            k++;
-        }
-    }else if(suma == true){
-        ans1.innerText = n1 + n2;
-        suma=false;
-        guardar[k] = (n1+" + "+n2+" = "+(n1+n2));
-        k++;
-    }else if(resta == true){
-        ans1.innerText = n1 - n2;
-        resta=false;
-        guardar[k] = (n1+" - "+n2+" = "+(n1-n2));
-        k++;
-    }else if(multiplicacion == true){
-        ans1.innerText = n1 * n2;
-        multiplicacion=false;
-        guardar[k] = (n1+" x "+n2+" = "+(n1*n2));
-        k++;
-       }
-
-})
-
-
-ce.addEventListener("click", function () {
-    pantalla.value = "0"
-    ans1.innerText = "";
-    i = 0;
-    n1 = 0;
-    n2 = 0;
-    pnt = false;
-})
-
-ac.addEventListener("click", function () {
-    pantalla.value = cantidad[i - 1];
-    i--
-    if (i < 0) {
-        pantalla.value = "0";
-        ans1.innerText = "";
-        n1 = 0;
-        n2 = 0;
-        pnt = false;
-    }
-})
-
-
-//Numeros
-point.addEventListener("click", function(){
-    if(pnt==false){
-        numero(point);
-        pnt=true;
-    }
-    
-})
-zero.addEventListener("click", function () {
-    numero(zero);
-})
-one.addEventListener("click", function () {
-    numero(one);
-})
-two.addEventListener("click", function () {
-    numero(two);
-})
-three.addEventListener("click", function () {
-    numero(three);
-})
-four.addEventListener("click", function () {
-    numero(four);
-})
-five.addEventListener("click", function () {
-    numero(five);
-})
-six.addEventListener("click", function () {
-    numero(six);
-})
-seven.addEventListener("click", function () {
-    numero(seven);
-})
-eight.addEventListener("click", function () {
-    numero(eight);
-})
-nine.addEventListener("click", function () {
-    numero(nine);
-})
-
-//Punto 2
-var input = document.getElementById("screen2"); //input inicial
-var answ = document.getElementById("resps"); //resultado
-var upca = document.getElementById("upc");
-var lc = document.getElementById("lowc");
-var input_ct = document.getElementById("cont");
-
-upca.addEventListener("click", function () {
-    input_val = input.value;
-    input_val = input_val.toUpperCase();
-    answ.innerText = input_val;
-})
-
-lc.addEventListener("click", function () {
-    input_val = input.value;
-    input_val = input_val.toLowerCase();
-    answ.innerText = input_val;
-})
-
-input_ct.addEventListener("click", function () {
-    input_val = input.value;
-    input_val = input_val.length;
-    answ.innerHTML = input_val;
-})
-
-
-//local storage
-
-cleann = document.getElementById("vaciar");
-
-eq.addEventListener("click", function () {
-    localStorage.setItem("dato", guardar);
-    beefore.innerHTML = localStorage.getItem("dato");
-    
-
 });
 
-vaciar.addEventListener("click",function(){
-var option = confirm("¿Seguro que quiere eliminar los datos guardados?");
-if(option==true){
-k=0;
-guardar = []
-beefore.innerHTML = "";
-}
+// Operators
 
+const operadores = botones.filter(element => {
+    if (simbolos.includes((element.value))) {
+        return element;
+    }
+});
+
+var n1 = "0";
+var n2 = "0";
+
+// Other buttons
+
+const otros = botones.filter(element => {
+    if (!simbolos.includes((element.value)) && !numeros.includes(parseInt(element.value))) {
+        return element;
+    }
+});
+
+var fullOperation = "";
+var firstNumber = true; //It allows to accumulate numbers in first screen
+var position = "first"; //Indicates the addend position
+var actualFunction = "";
+
+// Screens
+
+const pantallas = document.querySelectorAll(".scre");
+
+// Events
+
+numBut.forEach(element => {
+    element.addEventListener("click", () => {
+        if (element.value == "." && n1.includes(".") || n2.includes(".")) {
+            console.log("puntito");
+        } else if (position == "first") {
+            if (!firstNumber) {
+                n1 += element.value;
+            } else {
+                n1 = element.value;
+                firstNumber = false;
+            }
+            pantallas[1].value = n1;
+        } else {
+            if (!firstNumber) {
+                n2 += element.value;
+            } else {
+                n2 = element.value;
+                firstNumber = false;
+            }
+            pantallas[1].value = n2;
+        }
+    });
+});
+
+operadores.forEach(element => {
+    element.addEventListener("click", () => {
+        if (position == "first") {
+            position = "second";
+            pantallas[1].value = n2;
+            firstNumber = true;
+            fullOperation = n1 + element.value;
+            pantallas[0].value = fullOperation;
+            actualFunction = element.value;
+        } else {
+            if (n2 != "0") {
+                if (n2 == "0" && actualFunction == "/") {
+                    pantallas[1].value = "No se puede dividir entre 0";
+                    setTimeout(() => pantallas[1].value = n2, 1500);
+                } else {
+                    fullOperation += n2 + " = " + operar(n1, n2, actualFunction) + element.value + " ";
+                    pantallas[0].value = fullOperation;
+                    pantallas[1].value = "0";
+                    n1 = operar(n1, n2, actualFunction);
+                    n2 = "";
+                    position = "second";
+                    actualFunction = element.value;
+                };
+            }
+        }
+    });
+});
+
+otros[2].addEventListener("click", () => {
+    if (position == "first") {
+        n1 = (n1 / 100).toString();
+        pantallas[1].value = n1;
+    } else {
+        n2 = (n2 / 100).toString();
+        pantallas[1].value = n2;
+    }
 })
 
+function vaciar() {
+    n1 = "0";
+    n2 = "0";
+    firstNumber = true;
+    position = "first";
+    pantallas[0].value = "";
+    pantallas[1].value = "0";
+    fullOperation = "";
+}
+
+otros[1].addEventListener("click", () => { //Delete one
+    if (position == "first") {
+        if (n1.length <= 2) {
+            firstNumber = true;
+        };
+        if (n1.length == 1) {
+            vaciar();
+        };
+        if (n1.length >= 2) {
+            n1 = n1.substring(0, n1.length - 1);
+            pantallas[1].value = n1;
+        } else {
+            n1 = "0";
+            pantallas[1].value = n1;
+        }
+    } else {
+        if (n2.length <= 2) {
+            firstNumber = true;
+        };
+        if (n2.length == 1) {
+            vaciar();
+        };
+        if (n2.length >= 2) {
+            n2 = n2.substring(0, n2.length - 1);
+            pantallas[1].value = n2;
+        } else {
+            n2 = "0";
+            pantallas[1].value = n2;
+        }
+    };
+})
+
+otros[0].addEventListener("click", () => { //Delete everything
+    vaciar();
+});
+
+function operar(a, b, operacion) {
+    return new Function('return (' + a + ") " + operacion + "(" + b + ")")();
+}
+
+otros[4].addEventListener("click", () => { // Results
+    if (n2 != "0") {
+        if (n2 == "0" && actualFunction == "/") {
+            pantallas[1].value = "No se puede dividir entre 0";
+            setTimeout(() => pantallas[1].value = n2, 1500);
+        } else {
+            pantallas[0].value = "";
+            pantallas[1].value = operar(n1, n2, actualFunction);
+            fullOperation += n2 + " = " + operar(n1, n2, actualFunction);
+            n1 = (operar(n1, n2, actualFunction)).toString();
+            n2 = "";
+            position = "first";
+        };
+    }
+})
